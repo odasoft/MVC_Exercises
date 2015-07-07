750625f0ba51abb4eb432b4b5b393c8f8fee54e3
JDomingo7 <ing.domingoramirez@gmail.com>
2015-07-06 17:09:50 -0700

    Se agregaron metodos que regresan ordenes al proyecto del CHequeziel

--- PomodoroOrders/PomodoroOrders.Business/Repositorio_Orden.cs
+++ PomodoroOrders/PomodoroOrders.Business/Repositorio_Orden.cs
@@ -7,13 +7,25 @@ using System.Threading.Tasks;
 
 namespace PomodoroOrders.Business
 {
-    class Repositorio_Orden
+    public class Repositorio_Orden
     {
-        public Orden CrearOrden ()
+        public Orden Crear()
         {
-            Orden NuevaOrden = new Orden();
+            Orden NuevaOrden = new Orden() { IdOrden = 1, IdCarrito = 1 };
 
             return NuevaOrden;
+        }
+
+        public List<Orden> ObtenerLista()
+        {
+            List<Orden> Lista_Ordenes = new List<Orden>();
+
+            Lista_Ordenes.Add(new Orden { IdOrden = 1, IdCarrito = 1 });
+            Lista_Ordenes.Add(new Orden { IdOrden = 2, IdCarrito = 2 });
+            Lista_Ordenes.Add(new Orden { IdOrden = 3, IdCarrito = 3 });
+            Lista_Ordenes.Add(new Orden { IdOrden = 4, IdCarrito = 4 });
+
+            return Lista_Ordenes;
         }
     }
 }
